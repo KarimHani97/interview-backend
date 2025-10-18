91c946a99becb1a6cdac719e00e80733c6b6e97a
KarimHani97 <karimwilliam97@gmail.com>
2025-10-18 15:09:26 +0300

    Delete frontend directory

--- frontend/src/pages/Login.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../utils/AuthContext';
-
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, set
